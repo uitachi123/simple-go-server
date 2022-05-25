675f36f77c63fd0b3cd8e9ed1d2191b3fc32d014
uitachi123 <45985899+uitachi123@users.noreply.github.com>
2022-05-25 09:01:50 -0400

    make / a page also

--- main.go
+++ main.go
@@ -9,14 +9,19 @@ func welcome(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Welcome to new server!")
 }
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello!")
+}
+
 func goodbye(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Goodbye")
+	io.WriteString(w, "Goodbye!")
 }
 
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/welcome", welcome)
+	mux.HandleFunc("/", welcome)
+	mux.HandleFunc("/hello", hello)
 	mux.HandleFunc("/goodbye", goodbye)
 	// listen to port
 	http.ListenAndServe(":8080", mux)
