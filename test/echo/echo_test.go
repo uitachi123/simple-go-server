d5aad61dc2bb2990ecf18bc7e5c2b256943179fc
uitachi123 <45985899+uitachi123@users.noreply.github.com>
2022-05-29 12:21:06 -0400

    add simple test case

---
+++ test/echo/echo_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"go-server/pkg/echo"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_Echo(t *testing.T) {
+	req := httptest.NewRequest("GET", "/echo/hello", nil)
+	w := httptest.NewRecorder()
+
+	expectedOutput := "hello!"
+	h := http.HandlerFunc(echo.Echo)
+
+	h.ServeHTTP(w, req)
+	body := w.Body.String()
+
+	if body != expectedOutput {
+		t.Errorf("Expected %v\tGot %v", expectedOutput, body)
+	}
+}
