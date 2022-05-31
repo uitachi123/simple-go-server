daf4e999c481133736f6e3a5f9dab3480fa4a096
uitachi123 <45985899+uitachi123@users.noreply.github.com>
2022-05-30 21:44:24 -0400

    add in-memory db go-memdb

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,5 +1,34 @@
 package db
 
-func init() {
+import (
+	memdb "github.com/hashicorp/go-memdb"
+)
 
+type User struct {
+	Email string
+	Name  string
+}
+
+func Db() (*memdb.MemDB, error) {
+	// Create the DB schema
+	schema := &memdb.DBSchema{
+		Tables: map[string]*memdb.TableSchema{
+			"user": &memdb.TableSchema{
+				Name: "user",
+				Indexes: map[string]*memdb.IndexSchema{
+					"id": &memdb.IndexSchema{
+						Name:    "id",
+						Unique:  true,
+						Indexer: &memdb.StringFieldIndex{Field: "Email"},
+					},
+					"name": &memdb.IndexSchema{
+						Name:    "name",
+						Unique:  false,
+						Indexer: &memdb.StringFieldIndex{Field: "Name"},
+					},
+				},
+			},
+		},
+	}
+	return memdb.NewMemDB(schema)
 }
--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -1,1 +1,47 @@
 package db
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_Db(t *testing.T) {
+	db, err := Db()
+	if err != nil {
+		t.Errorf("Error creating database %v", err)
+	}
+	if db == nil {
+		panic("null db pointer")
+	}
+	txn := db.Txn(true)
+	users := []*User{
+		&User{"bob@test.com", "Bob"},
+		&User{"alice@test.com", "Alice"},
+	}
+	for _, u := range users {
+		if err := txn.Insert("user", u); err != nil {
+			t.Errorf("Error inserting user %v: %v", u, err)
+		}
+	}
+	txn.Commit()
+	txn = db.Txn(false)
+	defer txn.Abort()
+
+	// test id indexing
+	raw, err := txn.First("user", "id", "bob@test.com")
+	if err != nil {
+		t.Errorf("Error fetching user bob@test.com: %v", err)
+	}
+	if !reflect.DeepEqual(raw.(*User).Name, "Bob") {
+		t.Errorf("Data mismatch, expected:  %s got: %s", "Bob", raw.(*User).Name)
+	}
+
+	// testing
+	raw, err = txn.First("user", "name", "Alice")
+	if err != nil {
+		t.Errorf("Error fetching user bob@test.com: %v", err)
+	}
+	if !reflect.DeepEqual(raw.(*User).Email, "alice@test.com") {
+		t.Errorf("Data mismatch, expected:  %s got: %s", "alice@test.com", raw.(*User).Email)
+	}
+}
