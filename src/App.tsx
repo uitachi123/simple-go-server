d79c03d0efa4a841d606dd63bfb85621369c5010
uitachi123 <45985899+uitachi123@users.noreply.github.com>
2022-06-23 23:36:55 -0400

    bind button to fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 import { Button } from 'react-bootstrap';
 
+const listUsers = () => {
+  console.log("fetch api")
+  fetch("/users")
+    .then((response) => console.log(response))
+  // .then((json) => {
+  //   console.log(json);
+  // });
+}
+
 function App() {
   return (
     <div className="App">
-      <Button>Click to show users</Button>
+      <Button onClick={listUsers}>Click to show users</Button>
     </div>
   );
 }
