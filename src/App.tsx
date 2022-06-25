8a331516eb8a17ec03ee3474600fe2fd4e4fbe6f
uitachi123 <45985899+uitachi123@users.noreply.github.com>
2022-06-25 14:48:31 -0400

    use dev proxy, now backend data can be fetched

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 
 const listUsers = () => {
   console.log("fetch api")
   fetch("/users")
-    .then((response) => console.log(response))
-  // .then((json) => {
-  //   console.log(json);
-  // });
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+    });
 }
 
 function App() {
   return (
     <div className="App">
       <Button onClick={listUsers}>Click to show users</Button>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </Table>
     </div>
   );
 }
