de54d7ecd25e88debd8d7f6fc381f741fc21c2a4
uitachi123 <45985899+uitachi123@users.noreply.github.com>
2022-06-25 17:06:03 -0400

    show table only when data is fetced

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import { Button, Table } from 'react-bootstrap';
 
-const listUsers = () => {
-  console.log("fetch api")
-  fetch("/users")
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-    });
-}
-
 function App() {
+  const [show, setShow] = useState(false);
+  // const [data, setData] = useState
+  const listUsers = () => {
+    console.log("fetch api")
+    fetch("/users")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setShow(true)
+      });
+  }
+  let table = <div></div>
+  if (show) {
+    table = (
+      <div className="AppInfo">
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </Table>
+      </div>
+    )
+  }
   return (
     <div className="App">
       <Button onClick={listUsers}>Click to show users</Button>
-      <Table striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-        </tbody>
-      </Table>
+      {table}
     </div>
   );
 }
