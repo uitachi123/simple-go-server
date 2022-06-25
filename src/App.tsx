a9cdcd825a7f5a3d721501e8ab3c13314771b92f
uitachi123 <45985899+uitachi123@users.noreply.github.com>
2022-06-25 17:15:21 -0400

    render user info successfully

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import { useState } from 'react';
 import './App.css';
 import { Button, Table } from 'react-bootstrap';
 
+interface User {
+  name: string
+  email: string
+}
+
 function App() {
-  const [show, setShow] = useState(false);
-  // const [data, setData] = useState
+  const [show, setShow] = useState(false)
+  const [data, setData] = useState([] as User[])
   const listUsers = () => {
     console.log("fetch api")
     fetch("/users")
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
+        setData(json)
         setShow(true)
       });
   }
@@ -27,6 +33,13 @@ function App() {
             </tr>
           </thead>
           <tbody>
+            {data.map((user, index) =>
+              <tr key={index}>
+                <th>{index}</th>
+                <th>{user.name}</th>
+                <th>{user.email}</th>
+              </tr>
+            )}
           </tbody>
         </Table>
       </div>
